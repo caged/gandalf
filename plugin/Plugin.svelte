d3c64e253d4b941d51f41f418d336c4b10cb55a5
Justin Palmer <encytemedia@gmail.com>
2021-11-08 11:03:32 -0800

    fix capitalization for polyfillNode

--- plugin/Plugin.svelte
+++ plugin/Plugin.svelte
@@ -1,4 +1,8 @@
 <script>
+  import {
+    ElementaryPluginRenderer as core,
+    el,
+  } from "@nick-thompson/elementary";
 </script>
 
 <h2>Hello world</h2>
--- snowpack.config.mjs
+++ snowpack.config.mjs
@@ -25,6 +25,7 @@ export default {
     // "bundle": true,
   },
   packageOptions: {
+    polyfillNode: true,
     /* ... */
   },
   devOptions: {
